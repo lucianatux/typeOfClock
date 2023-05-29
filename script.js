c083189f0e65c6b1a429fb479b6f121bd6617324
lucianatux <lucianatux@yahoo.com.ar>
2023-05-29 16:42:49 -0300

    estilos

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
 function App(){
-    return <div className="wrap">
-        <div className="container">
-            <h2 id="title">Clock</h2>
-            <i className="fa fa-clock-o"></i>
-            <div className="break length-control">
-                <div id="break-label">Break Label</div>
-                <div className="controls">
-                    <div id="break-decrement">
-                    <i className="fa fa-arrow-down fa-2x"></i>
+    return <div className="main-wrapper">
+        <div className="title-wrapper">
+                <h2 id="title">Clock</h2>
+                <i className="fa fa-clock-o"></i>
+        </div>
+        <div className="wrapper">
+            <div className="length-control-wrapper">
+                <div className="break length-control">
+                    <div id="break-label">Break Label</div>
+                    <div className="controls">
+                        <button id="break-decrement">
+                            <i className="fa fa-arrow-down fa-2x"></i>
+                        </button>
+                        <div id="break-length">5</div>
+                        <button id="break-increment">
+                            <i className="fa fa-arrow-up fa-2x"></i>
+                        </button>
+                    </div>                
+                </div>
+                <div className="session length-control">
+                    <div id="session-label">Session Length</div>
+                    <div className="controls">
+                        <button id="session-decrement">
+                            <i className="fa fa-arrow-down fa-2x"></i>
+                        </button>
+                        <div id="session-length">25</div>
+                        <button id="session-increment">
+                            <i className="fa fa-arrow-up fa-2x"></i>
+                        </button>
                     </div>
-                    <div id="break-length">5</div>
-                    <div id="break-increment">
-                    <i className="fa fa-arrow-up fa-2x"></i>
-                    </div>
-                </div>                
-            </div>
-            <div className="session length-control">
-                <div id="session-label">Session Length</div>
-                <div className="controls">
-                    <div id="session-decrement"></div>
-                    <div id="session-length">25</div>
-                    <div id="session-increment"></div>
                 </div>
             </div>
+            <div className="timer-wrapper">
+                <div id="timer-label">Session</div>
+                <div id="time-left">11:11</div>
+                <div className="timer-control">
+                    <button id="start_stop">
+                        <i className="fa fa-play fa-2x"></i>
+                        <i className="fa fa-pause fa-2x"></i>
+                    </button>
+                    <button id="reset">
+                        <i className="fa fa-refresh fa-2x"></i>
+                    </button>
+                </div>
+            </div>
+            
         </div>
     </div>
 }
